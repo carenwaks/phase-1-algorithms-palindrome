59141e6321f806c50fb68d1701ab0fc0c50c578d
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-04-06 10:16:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray=word.split('')
+  const reverseWordArray= wordArray.reverse()
+  const reverseWord = reverseWordArray.join('')
+   if (word === reverseWord) {
+    return true
+   } else {
+    return false
+   }  
 }
 
+
 /* 
-  Add your pseudocode here
+ Add your pseudocode here
+  function isPalindrome(word) {
+    reverse the word 
+    test whether word is palindrome
+    if function on test to return true or false dependeng on the test result
+  }
 */
+
 
 /*
   Add written explanation of your solution here
+  The word is first split and turned to an array.Each letter in the word is assigned an index.
+  The index of the word are then reversed in order and joined to form a new word
+  The new word is compared to the original word and tested on whether they are simillar.
+  If simillar, true is returned, otherwise false is returned 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log(isPalindrome('mom'))
+  console.log(isPalindrome('woman'));
 }
 
 module.exports = isPalindrome;
